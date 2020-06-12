21418539dcc6aa2207426830b2b76fd3722d28e0
douglas17 <douglas@lar.app>
2020-06-12 09:15:38 -0300

    Add AuditorAware

--- desafio/src/main/java/com/codenation/desafio/desafio/DesafioApplication.java
+++ desafio/src/main/java/com/codenation/desafio/desafio/DesafioApplication.java
@@ -1,15 +1,24 @@
 package com.codenation.desafio.desafio;
 
+import com.codenation.desafio.desafio.security.SpringSecurityAuditorAware;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class DesafioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioApplication.class, args);
+	}
+
+	@Bean
+	AuditorAware<String> auditorProvider() {
+		return new SpringSecurityAuditorAware();
 	}
 
 }
