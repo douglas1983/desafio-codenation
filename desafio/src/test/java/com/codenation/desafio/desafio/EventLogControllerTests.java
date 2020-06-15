f677ca8839b972bcb0010d36f5ce7b2e19137257
douglas17 <douglas@lar.app>
2020-06-15 09:37:27 -0300

    caminho para @sql teste

--- desafio/src/test/java/com/codenation/desafio/desafio/EventLogControllerTests.java
+++ desafio/src/test/java/com/codenation/desafio/desafio/EventLogControllerTests.java
@@ -41,7 +41,7 @@ import org.springframework.web.context.WebApplicationContext;
 @RunWith(SpringRunner.class)
 @WebAppConfiguration
 @SpringBootTest(classes = DesafioApplication.class)
-@Sql({ "pre-sql.sql" })
+@Sql("pre-sql.sql")
 public class EventLogControllerTests {
 
   private static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(),
